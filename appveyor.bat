8a2e3796b3433f2a75f7724c2d86a0a9ba0e9a3c
Dejan Budimir <dejbug@users.noreply.github.com>
2017-08-10 02:51:24 +0200

    fixing Appveyor logic #5

--- appveyor.bat
+++ appveyor.bat
@@ -33,38 +33,46 @@ ECHO -- [ running ] "stage: build"
 
 ECHO -- selecting build target
 
-IF "%APPVEYOR_REPO_TAG%"=="true" GOTO build_release
-IF "%APPVEYOR_REPO_TAG%"=="false" GOTO build_debug
+SET MAKEFLAGS=WINFLAGS=--wincompat FLEX=C:\ProgramData\chocolatey\lib\winflexbison\tools\win_flex.exe BISON=C:\ProgramData\chocolatey\lib\winflexbison\tools\win_bison.exe
+
+IF "%APPVEYOR_REPO_TAG%"=="true" (
+	ECHO -- building for release
+	SET MAKEFLAGS=%MAKEFLAGS% TARGET=release
+)
+IF "%APPVEYOR_REPO_TAG%"=="false" (
+	ECHO -- building for debug
+	SET MAKEFLAGS=%MAKEFLAGS% TARGET=debug
+)
+ECHO -- [ ERROR ] unexpected build type
 GOTO :EOF
 
-REM REM REM REM REM REM REM REM REM REM REM REM REM REM REM REM REM REM
 
-:build_debug
+PUSHD src
+make %MAKEFLAGS%
+POPD
 
-ECHO -- building for debug
+ECHO -- generating build tree
+IF NOT EXIST build MKDIR build
 
-make TARGET=debug WINFLAGS=--wincompat
+ECHO -- moving files
+MOVE src\protocasm.exe build\protocasm.exe
+python src\patch.py src\osm.protocasm -f -o build\osm.protocasm -b Ii4uXGRhdGFcRGFybXN0YWR0Lm9zbS5wYmYi IlBobm9tUGVuaC5vc20ucGJmIg==
 
-GOTO run_test
+ECHO -- writing scripts
+ECHO IF NOT EXIST %%~dp0\$(notdir $(TEST_PBF_URL)) wget -P %%~dp0 --no-check-certificate $(TEST_PBF_URL) > build\fetch_sample.bat
+ECHO CALL %%~dp0\fetch_sample.bat > build\run_test.bat
+ECHO %%~dp0\protocasm.exe %%~dp0\osm.protocasm >> build\run_test.bat
 
-REM REM REM REM REM REM REM REM REM REM REM REM REM REM REM REM REM REM
+ECHO -- writing info
+ECHO [InternetShortcut] > build\protocasm.url
+ECHO URL=https://github.com/dejbug/protocasm >> build\protocasm.url
 
-:build_release
-
-ECHO -- building for release
-
-make TARGET=release WINFLAGS=--wincompat
-
-ECHO %APPVEYOR_PROJECT_NAME%-%APPVEYOR_REPO_TAG_NAME%.zip > build\release.txt
-
-ECHO -- packing output to deploy
-7z a deploy\%APPVEYOR_PROJECT_NAME%-%APPVEYOR_REPO_TAG_NAME%.zip build\*
-
-DIR deploy
-
-GOTO run_test
-
-:run_test
+IF "%APPVEYOR_REPO_TAG%"=="true" (
+	ECHO -- packing output to deploy
+	ECHO %APPVEYOR_PROJECT_NAME%-%APPVEYOR_REPO_TAG_NAME%.zip > build\release.txt
+	7z a deploy\%APPVEYOR_PROJECT_NAME%-%APPVEYOR_REPO_TAG_NAME%.zip build\*
+	DIR deploy
+)
 
 ECHO -- running test
 PUSHD build
@@ -74,5 +82,3 @@ POPD
 ECHO -- [WARN] keeping sample file in build folder
 REM ECHO -- deleting sample file in build folder
 REM DEL build\*.osm.pbf 2>NUL
-
-GOTO :EOF
